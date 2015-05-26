15be0eaa3bca5b0e6d6425216ac6a5f801423484
danixoid <danixoid@gmail.com>
2015-05-26 19:39:40 +0600

    Все что есть

--- config/oauth-5-laravel.php
+++ config/oauth-5-laravel.php
@@ -29,13 +29,13 @@ return [
         'Facebook' => [
             'client_id'     => '836370199744963',
             'client_secret' => 'e1c320ace18d6bf34efe1649bcbcbec1',
-            'scope'         => ['email','read_friendlists','user_online_presence'],
+            'scope'         => ['email','read_custom_friendlists','user_about_me'],
         ],
 
         'Google' => [
             'client_id'     => '794016713921-tiinncut6k3ondqk6spifqd3g04i2ru6.apps.googleusercontent.com',
             'client_secret' => 'hrMRKQNT163tuvFamtVbgudw',
-            'scope'         => ['email', 'public_profile'],
+            'scope'         => ['userinfo_email', 'userinfo_profile'],
         ],
     ]
 
