01b6e45aabf1075996c19da9c104e07394ae35b5
danixoid <danixoid@gmail.com>
2015-08-14 21:23:05 +0600

    JSON Controller and models

--- app/Http/Controllers/JsonController.php
+++ app/Http/Controllers/JsonController.php
@@ -23,7 +23,7 @@ class JsonController extends Controller {
     public function __construct()
     {
         $this->middleware('auth');
-        //$this->middleware('mobile');
+        $this->middleware('mobile');
     }
 
 	/**
